068dd8dc4161cd728a11944834baf8451dd8943e
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2025-02-14 16:59:01 +0100

    order service v2 - wiremock added --> asynchronious communication added

--- src/main/java/com/online_shop/microservices/order_service/service/OrderService.java
+++ src/main/java/com/online_shop/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.online_shop.microservices.order_service.service;
 
+import com.online_shop.microservices.order_service.client.InventoryClient;
 import com.online_shop.microservices.order_service.dto.OrderRequest;
 import com.online_shop.microservices.order_service.model.Order;
 import com.online_shop.microservices.order_service.repository.OrderRepository;
@@ -13,15 +14,22 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        //map OrderRequest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        //save order to OrderRepository
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductInStock) {
+            //map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to OrderRepository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is out of stock");
+        }
     }
 }
